4736ebe1da7a9b19be76d02144a4b5448797d861
Claire McQuin <claire@getchef.com>
2014-10-30 15:34:29 -0700

    Update for chef with RSpec 3

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -52,6 +52,7 @@ override :cacerts, version: '2014.08.20'
 
 override :bundler,        version: "1.7.2"
 override :ruby,           version: "2.1.3"
+override :chef,           version: "mcquin/rspec-3"
 ######
 # Ruby 2.1.3 is currently not working on windows due to:
 # https://github.com/ffi/ffi/issues/375
